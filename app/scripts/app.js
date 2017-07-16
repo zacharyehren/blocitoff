aaa870a591f0211605ed4feff89d1f694ae0317b
Zachary Ehren <zachehren@gmail.com>
2017-07-16 14:42:22 -0700

    create login and register states

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,14 +12,34 @@
              controller: 'HomeCtrl as home',
              templateUrl: '/templates/home.html'
          })
+         .state('login', {
+           url: '/login',
+           templateUrl: 'templates/login.html',
+           controller: 'AuthCtrl',
+           onEnter: function(Auth, $state){
+             Auth.currentUser().then(function(){
+            $state.go('home')
+          })
+         }
+        })
+        .state('register', {
+          url: '/register',
+          templateUrl: 'templates/register.html',
+          controller: 'AuthCtrl',
+          onEnter: function(Auth, $state){
+          Auth.currentUser().then(function(){
+            $state.go('home')
+          })
+         }
+        })
          .state('users', {
            url: '/users',
            controller: 'UsersCtrl as users',
            templateUrl: '/templates/users.html'
-         });
+         })
    }
 
   angular
-    .module('blocitoff', ['ui.router'])
+    .module('blocitoff', ['ui.router', 'Devise'])
     .config(config);
 })();
