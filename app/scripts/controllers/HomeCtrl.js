47d0215f46de30675c035890d2328ca460aa335d
Ben Neely <ben.neely@kount.com>
2017-07-17 19:33:32 -0600

    Get some tasks

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,14 +1,10 @@
 (function() {
   function HomeCtrl(ApiRequests) {
+    this.ApiRequests = ApiRequests;
 
     this.create_task = function() {
       ApiRequests.create_task(this.task);
     }
-
-    window.onload = ApiRequests.list_tasks();
-    this.list_tasks = ApiRequests.tasks;
-    console.log(ApiRequests.tasks);
-
   }
 
   angular
