a4f54e1ce0d5ec65662390909f8d7885da193507
Zachary Ehren <zachehren@gmail.com>
2017-08-22 10:57:53 -0400

    styling updates, remove  function

--- app/scripts/controllers/ListCtrl.js
+++ app/scripts/controllers/ListCtrl.js
@@ -1,5 +1,5 @@
 (function() {
-  function ListCtrl(ApiRequests, $timeout, $cookies) {
+  function ListCtrl(ApiRequests, $cookies) {
 
     this.ApiRequests = ApiRequests;
 
@@ -8,14 +8,6 @@
     this.delete_task = function(task_id){
     ApiRequests.delete_task(task_id);
   };
-
-    this.hide_task = function(task_id){
-      if (task_id != null) {
-        $timeout(function(){
-          ApiRequests.delete_task(task_id)
-        }, 2000);
-      }
-    }
 
     this.create_task = function() {
       ApiRequests.create_task(this.task, this.list_id);
@@ -26,5 +18,5 @@
 
   angular
     .module('blocitoff')
-    .controller('ListCtrl', ['ApiRequests', '$timeout', '$cookies', ListCtrl]);
+    .controller('ListCtrl', ['ApiRequests', '$cookies', ListCtrl]);
 })();
