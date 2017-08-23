b95476c1bc5daa649317fec9047472d60bdb9d6e
Zachary Ehren <zachehren@gmail.com>
2017-08-23 10:31:06 -0400

    spacing refactor

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,35 +1,34 @@
 (function() {
   function config($locationProvider, $stateProvider, $routeProvider) {
-     $locationProvider
-         .html5Mode({
-             enabled: true,
-             requireBase: false
-          });
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
 
-     $stateProvider
-        .state('home', {
-             url: '/',
-             controller: 'HomeCtrl as home',
-             templateUrl: '/templates/home.html'
-         })
-         .state('users', {
-           url: '/users',
-           controller: 'UsersCtrl as users',
-           templateUrl: '/templates/users.html'
-         })
-         .state('list', {
-           url: '/list',
-           controller: 'ListCtrl as list',
-           templateUrl: '/templates/list.html'
-         });
+    $stateProvider
+      .state('home', {
+        url: '/',
+        controller: 'HomeCtrl as home',
+        templateUrl: '/templates/home.html'
+      })
+      .state('users', {
+        url: '/users',
+        controller: 'UsersCtrl as users',
+        templateUrl: '/templates/users.html'
+      })
+      .state('list', {
+        url: '/list',
+        controller: 'ListCtrl as list',
+        templateUrl: '/templates/list.html'
+      });
 
-      $routeProvider
-        .when('/lists/:id', {
-          controller: 'ListCtrl as list'
-        });
+    $routeProvider
+      .when('/lists/:id', {
+        controller: 'ListCtrl as list'
+      });
 
-
-   }
+  }
 
   angular
     .module('blocitoff', ['ui.router', 'ui.bootstrap', 'ngCookies', 'ngRoute'])
